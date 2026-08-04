d0d7576ec0dbbe3571d51ab3a7ab72335057c0d0
Nikhil Ranjan Tripathi <134865662+Nikhil-Ranjan-Tripathi@users.noreply.github.com>
2026-08-04 16:23:01 +0530

    Create notion.py

---
+++ Speech_Rec/notion.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+
+class NotionClient:
+
+    def __init__(self, token, database_id) -> None:
+        self.database_id = database_id
+
+        self.headers = {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json",
+            "Notion-Version": "2021-08-16"
+        }
